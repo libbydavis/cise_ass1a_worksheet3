5396fdbdc46e1ba8c898984ce2eef397b286669f
libbydavis <libstarnz@gmail.com>
2022-03-24 15:19:30 +1300

    fixed port and procfile

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.get('/', (req, res) => res.send('Hello world!'));
 // use Routes
 app.use('/api/books', books);
 
-const PORT = process.env.PORT || 5000
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
