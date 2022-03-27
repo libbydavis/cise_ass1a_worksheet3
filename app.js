ba86dd55cb009f04b1002107f1198ba049431f88
libbydavis <libstarnz@gmail.com>
2022-03-28 12:12:34 +1300

    added path back

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // app.js
-/*
 
 const express = require('express');
 const connectDB = require('./config/db');
@@ -32,42 +31,10 @@ if (port == null || port == "") {
 const path = require("path");
 
 // Step 1:
-app.use(express.static(path.join(__dirname, "./mern-app/build")));
+app.use(express.static(path.join(__dirname, 'mern-app', 'build')));
 // Step 2:
-app.get("/*", function (request, response) {
-    response.sendFile(path.join(__dirname, "./mern-app/build", "index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'mern-app', 'build', 'index.html'))
 });
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
- */
-// app.js
-
-const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
-
-// routes
-const books = require('./routes/api/books');
-
-const app = express();
-
-// Connect Database
-connectDB();
-
-// cors
-app.use(cors({ origin: true, credentials: true }));
-
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-
-// use Routes
-app.use('/api/books', books);
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 8000;
-}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
